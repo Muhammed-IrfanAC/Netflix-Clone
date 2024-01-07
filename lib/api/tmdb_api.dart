62e66c190f20a4d219a23b2368b96cd5d1f82866
Muhammed Irfan <acmi.official12@gmail.com>
2024-01-07 15:32:23 +0530

    bearer token and keys issue fixed

--- lib/api/tmdb_api.dart
+++ lib/api/tmdb_api.dart
@@ -4,9 +4,6 @@ import 'package:http/http.dart' as http;
 import 'package:netflix_clone/api/constants.dart';
 import 'package:netflix_clone/models/movielist_model.dart';
 
-var headers={"Authorization":"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxM2I1Yzc5YmIwZWY2YTQwNDIyNmVlMjE1NTU4YzVmNiIsInN1YiI6IjY1OTdkOTZmNzI2ZmIxMTY0YWFiMjg3NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JSDp1JYDNz1h_wP5953V6bcYdJ0mdAMHaqNghSNBeUk",
-"accept":"application/json"};
-
 class TmdbApi {
   Future<List<MovieListModel>> fetchMovies(String category) async {
     final url= Uri.parse('${Constants.baseUrl}$category${Constants.apiKey}');
